e89c2f5c157370c8a68017228b2f358ec6920503
Lorenzo <lsandini@gmail.com>
2024-07-25 19:45:36 +0300

    fix

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,242 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import * as React from "react";
+import { Button, StyleSheet, View } from "react-native";
+import {
+  aggregateRecord,
+  getGrantedPermissions,
+  initialize,
+  insertRecords,
+  getSdkStatus,
+  readRecords,
+  requestPermission,
+  revokeAllPermissions,
+  SdkAvailabilityStatus,
+  openHealthConnectSettings,
+  openHealthConnectDataManagement,
+  readRecord,
+} from "react-native-health-connect";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const getLastWeekDate = (): Date => {
+  return new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000);
+};
 
-export default function HomeScreen() {
+const getLastTwoWeeksDate = (): Date => {
+  return new Date(new Date().getTime() - 2 * 7 * 24 * 60 * 60 * 1000);
+};
+
+const getTodayDate = (): Date => {
+  return new Date();
+};
+
+export default function App() {
+  const initializeHealthConnect = async () => {
+    const result = await initialize();
+    console.log({ result });
+  };
+
+  const checkAvailability = async () => {
+    const status = await getSdkStatus();
+    if (status === SdkAvailabilityStatus.SDK_AVAILABLE) {
+      console.log("SDK is available");
+    }
+
+    if (status === SdkAvailabilityStatus.SDK_UNAVAILABLE) {
+      console.log("SDK is not available");
+    }
+
+    if (
+      status === SdkAvailabilityStatus.SDK_UNAVAILABLE_PROVIDER_UPDATE_REQUIRED
+    ) {
+      console.log("SDK is not available, provider update required");
+    }
+  };
+
+  const insertSampleData = () => {
+    insertRecords([
+      {
+        recordType: "Steps",
+        count: 1000,
+        startTime: getLastWeekDate().toISOString(),
+        endTime: getTodayDate().toISOString(),
+      },
+    ])
+      .then((ids) => {
+        console.log("Records inserted ", { ids });
+      })
+      .catch((err) => {
+        console.error("Error inserting records ", { err });
+      });
+  };
+
+  const readSampleData = () => {
+    readRecords("Steps", {
+      timeRangeFilter: {
+        operator: "between",
+        startTime: getLastTwoWeeksDate().toISOString(),
+        endTime: getTodayDate().toISOString(),
+      },
+    })
+      .then((result) => {
+        console.log("Retrieved records: ", JSON.stringify({ result }, null, 2));
+      })
+      .catch((err) => {
+        console.error("Error reading records ", { err });
+      });
+  };
+
+  const readSampleDataSingle = () => {
+    readRecord("Steps", "a7bdea65-86ce-4eb2-a9ef-a87e6a7d9df2")
+      .then((result) => {
+        console.log("Retrieved record: ", JSON.stringify({ result }, null, 2));
+      })
+      .catch((err) => {
+        console.error("Error reading record ", { err });
+      });
+  };
+
+  const aggregateSampleData = () => {
+    aggregateRecord({
+      recordType: "Steps",
+      timeRangeFilter: {
+        operator: "between",
+        startTime: getLastWeekDate().toISOString(),
+        endTime: getTodayDate().toISOString(),
+      },
+    }).then((result) => {
+      console.log("Aggregated record: ", { result });
+    });
+  };
+
+  const requestSamplePermissions = () => {
+    requestPermission([
+      {
+        accessType: "read",
+        recordType: "Steps",
+      },
+      {
+        accessType: "write",
+        recordType: "Steps",
+      },
+    ]).then((permissions) => {
+      console.log("Granted permissions on request ", { permissions });
+    });
+  };
+
+  const grantedPermissions = () => {
+    getGrantedPermissions().then((permissions) => {
+      console.log("Granted permissions ", { permissions });
+    });
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <Button title="Initialize" onPress={initializeHealthConnect} />
+      <Button
+        title="Open Health Connect settings"
+        onPress={openHealthConnectSettings}
+      />
+      <Button
+        title="Open Health Connect data management"
+        onPress={() => openHealthConnectDataManagement()}
+      />
+      <Button title="Check availability" onPress={checkAvailability} />
+      <Button
+        title="Request sample permissions"
+        onPress={requestSamplePermissions}
+      />
+      <Button title="Get granted permissions" onPress={grantedPermissions} />
+      <Button title="Revoke all permissions" onPress={revokeAllPermissions} />
+      <Button title="Insert sample data" onPress={insertSampleData} />
+      <Button title="Read sample data" onPress={readSampleData} />
+      <Button title="Read specific data" onPress={readSampleDataSingle} />
+      <Button title="Aggregate sample data" onPress={aggregateSampleData} />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    rowGap: 16,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
   },
 });
+
+
+
+
+// import { Image, StyleSheet, Platform } from 'react-native';
+
+// import { HelloWave } from '@/components/HelloWave';
+// import ParallaxScrollView from '@/components/ParallaxScrollView';
+// import { ThemedText } from '@/components/ThemedText';
+// import { ThemedView } from '@/components/ThemedView';
+
+// export default function HomeScreen() {
+//   return (
+//     <ParallaxScrollView
+//       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+//       headerImage={
+//         <Image
+//           source={require('@/assets/images/partial-react-logo.png')}
+//           style={styles.reactLogo}
+//         />
+//       }>
+//       <ThemedView style={styles.titleContainer}>
+//         <ThemedText type="title">Welcome!</ThemedText>
+//         <HelloWave />
+//       </ThemedView>
+//       <ThemedView style={styles.stepContainer}>
+//         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+//         <ThemedText>
+//           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+//           Press{' '}
+//           <ThemedText type="defaultSemiBold">
+//             {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
+//           </ThemedText>{' '}
+//           to open developer tools.
+//         </ThemedText>
+//       </ThemedView>
+//       <ThemedView style={styles.stepContainer}>
+//         <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+//         <ThemedText>
+//           Tap the Explore tab to learn more about what's included in this starter app.
+//         </ThemedText>
+//       </ThemedView>
+//       <ThemedView style={styles.stepContainer}>
+//         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+//         <ThemedText>
+//           When you're ready, run{' '}
+//           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+//           <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
+//           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
+//           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+//         </ThemedText>
+//       </ThemedView>
+//     </ParallaxScrollView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   titleContainer: {
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     gap: 8,
+//   },
+//   stepContainer: {
+//     gap: 8,
+//     marginBottom: 8,
+//   },
+//   reactLogo: {
+//     height: 178,
+//     width: 290,
+//     bottom: 0,
+//     left: 0,
+//     position: 'absolute',
+//   },
+// });
